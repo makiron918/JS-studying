87614a4d4e5a2ee8f76ef7ae9bccec92f781f9eb
makiron918 <gonzaress918@gmail.com>
2020-04-19 10:14:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,12 @@ job = 'teacher';
 isMarried = false;
 
 console.log(firstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
