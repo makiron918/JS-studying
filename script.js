0a5943ca9d085ee5ae77c15b508ae525d871c322
makiron918 <gonzaress918@gmail.com>
2020-04-19 10:02:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ var firstName = 'John';
 console.log(firstName);
 
 var lastName = 'Smith';
-console.log(lastName);
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+job = 'Teacher';
+console.log(job);
