3e99d3fe07ee0f821e7ed05366b9c673b6d2aabc
makiron918 <gonzaress918@gmail.com>
2020-04-19 10:37:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -67,3 +67,20 @@ var fullAge = 18;
 
 var isFullAge = now - yearJohn >= fullAge;
 console.log(isFullAge);
+
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+var x, y;
+x = y = (3 + 5) * 4 - 6;
+console.log(x, y);
+
+
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+x++;
+console.log(x);
