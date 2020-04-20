e4efdd885316e6e3da1488a4baf3f9a7bacb6ab2
makiron918 <gonzaress918@gmail.com>
2020-04-20 20:26:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -107,3 +107,15 @@ if (civilStatus === 'married') {
 } else {
   console.log(firstName + ' will hopefully marry soon :)');
 }
+
+
+var firstName = 'John';
+var age = 16;
+
+if (age < 13){
+  console.log(firstName + ' is a boy.');
+} else if (age >= 13 && age < 20) {
+  console.log(firstName + ' is a teenager.')
+} else {
+  console.log(firstName + ' is a man.');
+}
