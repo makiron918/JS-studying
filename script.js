650167d8cd847c9cd3512f58df4b42dd661bfd80
makiron918 <gonzaress918@gmail.com>
2020-04-22 10:25:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -198,3 +198,11 @@ console.log(whatDoYouDo('retired', 'Mark'));
 
 var names = ['John', 'Mark', 'Jane'];
 var years = new Array(1990, 1969, 1948);
+
+console.log(names);
+console.log(names.length);
+
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+console.log(names);
+
