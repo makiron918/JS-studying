f510515fc4e8b9bb0c4d816cc769d2f68ae6a21c
makiron918 <gonzaress918@gmail.com>
2020-04-22 10:18:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -175,3 +175,22 @@ function yearsUntilRetirement(year, firstName) {
 yearsUntilRetirement(1990, 'John');
 yearsUntilRetirement(1948, 'Mike');
 yearsUntilRetirement(1968, 'Jane');
+
+
+
+var whatDoYouDo = function(job, firstName) {
+  switch(job) {
+    case 'teacher':
+      return firstName + ' teaches kids how to code';
+    case 'driver':
+      return firstName + ' drives a cab in Lisbon';
+    case 'designer':
+      return firstName + ' designes beautiful websites';
+    default:
+      return firstName + ' does something else';
+  }
+}
+
+console.log(whatDoYouDo('teacher', 'John'));
+console.log(whatDoYouDo('designer', 'Jane'));
+console.log(whatDoYouDo('retired', 'Mark'));
