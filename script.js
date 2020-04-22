99bfc757ff760f1d1114887602211e84a2d4ebf1
makiron918 <gonzaress918@gmail.com>
2020-04-22 11:49:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -221,3 +221,27 @@ console.log(John.indexOf(23));
 
 var isDesigner = John.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
 console.log(isDesigner);
+
+
+var john = {
+  firstName: 'John',
+  lastName: 'Smith',
+  birthYear: 1990,
+  family: ['Jane', 'Mark', 'Bob', 'Emily'],
+  job: 'teacher',
+  isMarried: false
+};
+console.log(john.firstName);
+console.log(john['lastName']);
+var x = 'birthYear';
+console.log(john[x]);
+
+john.job = 'designer';
+john['isMarried'] = true;
+console.log(john);
+
+var jane = new Object();
+jane.firstName = 'Jane';
+jane.birthYear = 1969;
+jane[lastName] = 'Smith';
+console.log(jane);
