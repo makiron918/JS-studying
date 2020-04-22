91364a1ef019ba56a8c295ed74b768d309634984
makiron918 <gonzaress918@gmail.com>
2020-04-22 11:57:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -245,3 +245,19 @@ jane.firstName = 'Jane';
 jane.birthYear = 1969;
 jane[lastName] = 'Smith';
 console.log(jane);
+
+
+var john = {
+  firstName: 'John',
+  lastName: 'Smith',
+  birthYear: 1992,
+  family: ['Jane', 'Mark', 'Bob', 'Emily'],
+  job: 'teacher',
+  isMarried: false,
+  calcAge: function() {
+    this.age = 2018 - this.birthYear;
+  }
+};
+john.calcAge();
+console.log(john);
+
